3b3a671574ca96ae8ba58b8cb179ff36e299b490
David Gilbertson <gilbertson.david@gmail.com>
2016-05-12 15:15:27 +1000

    Better analytics script

--- app/server.js
+++ app/server.js
@@ -26,28 +26,13 @@ const port = process.env.PORT || 8080;
 
 const fileNames = require('../build/stats/fileNames.json');
 
-const gaScript = `
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-77392102-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
-`;
-
 function getHtml(req, props) {
   let scriptSrc;
-  let googleAnalyticsScript = '';
 
   if (process.env.APP_ENV === 'DEV') {
     scriptSrc = 'http://localhost:8081/webpack-bundle.js';
   } else {
     scriptSrc = `/js/${fileNames.mainJs}`;
-    googleAnalyticsScript = gaScript;
   }
   
   const appHtml = renderToString(
@@ -79,13 +64,20 @@ function getHtml(req, props) {
           <script>window.MALLA_STATE=${JSON.stringify(store.getState())};</script>
           <script>window.MALLA_CONSTANTS=${JSON.stringify(MALLA_CONSTANTS)};</script>
           <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,300|Open+Sans:400,300' rel='stylesheet' type='text/css'>
+
+          <script>
+            window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+            ga('create', '${VENDORS.GOOGLE_ANALYTICS_TRACKING_ID}', 'auto');
+            ga('send', 'pageview');
+          </script>
+
+          <script async src='https://www.google-analytics.com/analytics.js'></script>
       </head>
       <body>
           <div id="app">${appHtml}</div>
       
           <script src="https://cdn.firebase.com/js/client/2.4.2/firebase.js"></script>
           <script async src="${scriptSrc}"></script>
-          ${googleAnalyticsScript}
       </body>
       </html>`;
 
