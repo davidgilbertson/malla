30e07ec335f1c365bce64af35c4fcc89876a7775
David Gilbertson <gilbertson.david@gmail.com>
2016-05-25 16:16:35 +1000

    Removed unused code

--- app/utils/snap.js
+++ app/utils/snap.js
@@ -1,10 +1,4 @@
-import {
-  GRID_SIZE,
-} from '../constants.js';
-
-// export function snap(num) {
-//   return Math.round(num / GRID_SIZE) * GRID_SIZE;
-// }
+import {GRID_SIZE} from '../constants.js';
 
 export function snap(num) {
   return Math.round(num / GRID_SIZE) * GRID_SIZE;
