3f9c37b885b6e32edc8d1b2c07172a4199dcbbb1
David Gilbertson <gilbertson.david@gmail.com>
2016-06-13 10:49:46 +1000

    Sync store to local storage

--- app/data/store.js
+++ app/data/store.js
@@ -6,42 +6,18 @@ import reducers from './reducers';
 import {
   ls,
 } from '../utils';
-import {
-  ACTIONS,
-} from '../constants.js';
 
 const onClient = typeof window !== 'undefined';
 
-function makeGlobalReducer(reducer) {
-  return function(state, action) {
-    switch (action.type) {
-      case ACTIONS.REPLACE_STATE:
-        return action.state;
+let localState;
 
-      default:
-        return reducer(state, action);
-    }
-  }
+if (onClient) {
+  const localStore = ls.load();
+
+  if (localStore) localState = localStore;
 }
 
-const globalReducer = makeGlobalReducer(reducers);
-
-const store = createStore(globalReducer);
-
-function populateFromLocalStorage(store) {
-  const localState = ls.load();
-
-  // next tick, just to avoid the react warning that markup is
-  // different on client and server. Probably a bad idea
-  setTimeout(() => {
-    if (localState) {
-      store.dispatch({
-        type: ACTIONS.REPLACE_STATE,
-        state: ls.load(),
-      });
-    }
-  });
-}
+const store = createStore(reducers, localState);
 
 let timer;
 
@@ -56,7 +32,6 @@ store.subscribe(() => {
 });
 
 if (onClient) {
-  populateFromLocalStorage(store);
   firebaseActions.init(store);
   firebaseWatcher.bindToStore(store);
 }
