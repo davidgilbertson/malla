05a137648a2ed9c578a4b4e0fa8b03f6673a0e39
David Gilbertson <gilbertson.david@gmail.com>
2016-06-17 08:15:09 +1000

    Simplify styles in panel

--- app/components/Panel/Panel.jsx
+++ app/components/Panel/Panel.jsx
@@ -12,60 +12,62 @@ import {
   css,
 } from '../../utils'
 
-const styles = {
-  panelWrapper: { // So that the body height is correct. https://github.com/philipwalton/flexbugs#3-min-height-on-a-flex-container-wont-apply-to-its-flex-items
-    display: 'flex',
-    flexFlow: 'column',
-  },
-  panel: {
-    flex: '0 0 auto',
-    display: 'flex',
-    flexFlow: 'column',
-    maxHeight: '94vh',
-    maxWidth: '98vw',
-    backgroundColor: COLORS.WHITE,
-    ...css.shadow(),
-  },
-  header: {
-    height: 64,
-    flex: '0 0 auto',
-    display: 'flex',
-    flexFlow: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    borderBottom: `3px solid ${COLORS.PRIMARY_LIGHT}`,
-    backgroundColor: COLORS.PRIMARY,
-    color: COLORS.WHITE,
-  },
-  title: {
-    fontSize: 22,
-    paddingLeft: 20,
-    whiteSpace: 'nowrap',
-    overflow: 'hidden',
-  },
-  close: {
-    padding: 20,
-    fontSize: 16,
-  },
-  body: {
-    flex: '1 1 auto',
-    padding: DIMENSIONS.SPACE_S,
-    overflow: 'auto',
-  },
-  actions: {
-    flex: '0 0 auto',
-    padding: 20,
-    textAlign: 'center',
-  },
-  okButton: {
-    backgroundColor: COLORS.PRIMARY,
-    color: COLORS.WHITE,
-    padding: 12,
-    minWidth: 100,
-  },
-};
-
 const Panel = props => {
+  const styles = {
+    panelWrapper: { // So that the body height is correct. https://github.com/philipwalton/flexbugs#3-min-height-on-a-flex-container-wont-apply-to-its-flex-items
+      display: 'flex',
+      flexFlow: 'column',
+    },
+    panel: {
+      flex: '0 0 auto',
+      display: 'flex',
+      flexFlow: 'column',
+      width: props.width,
+      maxHeight: '94vh',
+      maxWidth: '98vw',
+      backgroundColor: COLORS.WHITE,
+      ...css.shadow(),
+      ...props.style,
+    },
+    header: {
+      height: 64,
+      flex: '0 0 auto',
+      display: 'flex',
+      flexFlow: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      borderBottom: `3px solid ${COLORS.PRIMARY_LIGHT}`,
+      backgroundColor: COLORS.PRIMARY,
+      color: COLORS.WHITE,
+    },
+    title: {
+      fontSize: 22,
+      paddingLeft: 20,
+      whiteSpace: 'nowrap',
+      overflow: 'hidden',
+    },
+    close: {
+      padding: 20,
+      fontSize: 16,
+    },
+    body: {
+      flex: '1 1 auto',
+      padding: DIMENSIONS.SPACE_S,
+      overflow: 'auto',
+    },
+    actions: {
+      flex: '0 0 auto',
+      padding: 20,
+      textAlign: 'center',
+    },
+    okButton: {
+      backgroundColor: COLORS.PRIMARY,
+      color: COLORS.WHITE,
+      padding: 12,
+      minWidth: 100,
+    },
+  };
+
   const onOk = () => {
     props.onOk();
     props.hideModal();
@@ -94,15 +96,9 @@ const Panel = props => {
       </Button>
     ) : null;
 
-  const panelStyle = {
-    ...styles.panel,
-    width: props.width,
-    ...props.style,
-  };
-
   return (
     <div style={styles.panelWrapper}>
-      <div style={panelStyle}>
+      <div style={styles.panel}>
         <div style={styles.header}>
           <h1 style={styles.title}>
             {props.title}
