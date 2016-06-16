e311bcad8e04267d3fd4df240af7e2ae71a3edf3
David Gilbertson <gilbertson.david@gmail.com>
2016-06-16 17:03:29 +1000

    Added react perf tools

--- app/client.js
+++ app/client.js
@@ -4,6 +4,12 @@ require('./utils/normalizeNodeAndBrowser.js');
 
 require('./tracker.js').setBrowserDetails();
 
+// if (process.env.NODE_ENV !== 'production') {
+//   window.Perf = require('react-addons-perf');
+//   Perf.start();
+//   console.warn('React Perf is running. You should Perf.stop() it');
+// }
+
 import Root from './components/Root/Root.jsx';
 
 render(<Root />, document.getElementById('app'));
