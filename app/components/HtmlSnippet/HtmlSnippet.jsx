261adb560e429959cd22f1821af4430a095abc65
David Gilbertson <gilbertson.david@gmail.com>
2016-07-01 12:20:51 +1000

    Allow styling of HTML snippet

--- app/components/HtmlSnippet/HtmlSnippet.jsx
+++ app/components/HtmlSnippet/HtmlSnippet.jsx
@@ -6,10 +6,16 @@ const cleanHtml = html => sanitizeHtml(html, {
   allowedTags: sanitizeHtml.defaults.allowedTags.concat(['h1', 'h2', 'img', 'sup']),
 });
 
-const HtmlSnippet = props => <span dangerouslySetInnerHTML={{__html: cleanHtml(props.html)}} />;
+const HtmlSnippet = props => (
+  <span
+    style={props.style}
+    dangerouslySetInnerHTML={{__html: cleanHtml(props.html)}}
+  />
+);
 
 HtmlSnippet.propTypes = {
   html: PropTypes.string.isRequired,
+  style: PropTypes.object,
 };
 
 export default HtmlSnippet;
